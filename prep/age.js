265006018fc4a64f5940a772597080c04dd7c0b4
Fatimatou Bah <faatimabah@gmail.com>
2025-10-20 00:54:22 +0100

    add new file age.js to prep folder

---
+++ prep/age.js
@@ -0,0 +1,6 @@
+const yearOfBirth = 1995; //declaration
+let currentYear = 2025; //declaration
+
+currentYear++; //statement for incrementing year by 1
+const ageInString = `I am ${currentYear - yearOfBirth} years old` //assigning ageInString to the age using string
+console.log(ageInString); //output my current age
