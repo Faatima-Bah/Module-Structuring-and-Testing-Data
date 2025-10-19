f55ab61957d31866ce69c38d25fcea0e58a7d3cf
Fatimatou Bah <faatimabah@gmail.com>
2025-10-19 23:59:23 +0100

    working on rounding.js file in prep folder for coursework sprint 1

---
+++ prep/rounding.js
@@ -0,0 +1,11 @@
+//Rounding a value using the Math.round function and printing the output
+const roundedValue = Math.round(10.3);
+console.log(roundedValue);
+
+//Passing the rounded variable inside a string and printing
+const roundedValueInAString = `10.3 const roundedValue = Math.round(10.3); to ${Math.round(10.3)}`;
+console.log(roundedValueInAString);
+
+//Passing the rounded variable inside a string and printing
+const valueInString = `10.3 rounded is ${roundedValue}`;
+console.log(valueInString);
