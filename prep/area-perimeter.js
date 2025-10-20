090844f54c9e5aee18acd390d823392020dec60f
Fatimatou Bah <faatimabah@gmail.com>
2025-10-20 01:17:34 +0100

    added new file area-perimeter.js in the prep folder

---
+++ prep/area-perimeter.js
@@ -0,0 +1,13 @@
+const height = 10;
+const width = 30;
+
+//calculating the area
+const area = height * width;
+//print the area
+console.log(`the area is ${area}`);
+
+
+//calculating the perimeter
+const perimeter = (height + width) * 2;
+//print the perimeter
+console.log(`the perimeter is ${perimeter}`);
