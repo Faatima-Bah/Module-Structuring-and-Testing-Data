020bcf22e44d0b8beb7192e25ff4cc629351bfcf
Fatimatou Bah <faatimabah@gmail.com>
2025-10-20 23:10:49 +0100

    in area-perimeter.js functions have been created to help calculate

--- prep/area-perimeter.js
+++ prep/area-perimeter.js
@@ -1,13 +1,28 @@
-const height = 10;
-const width = 30;
+//let height = 10;
+//let width = 30;
 
 //calculating the area
-const area = height * width;
+
+function calculateArea(height, width) {
+    const area = height * width;
+    return area;
+}
+
+const areaResult1 = calculateArea(10,30);
+const areaResult2 = calculateArea(50,20);
 //print the area
-console.log(`the area is ${area}`);
+console.log(`The area is ${areaResult1}`);
+console.log(`The area is ${areaResult2}`);
 
 
 //calculating the perimeter
-const perimeter = (height + width) * 2;
+function calculatePerimeter(height,width) {
+    const perimeter = (height + width) * 2;
+    return perimeter
+}
+
+const perimeterResult1 = calculatePerimeter(10,30);
+const perimeterResult2 = calculatePerimeter(50,20);
 //print the perimeter
-console.log(`the perimeter is ${perimeter}`);
+console.log(`the perimeter is ${perimeterResult1}`);
+console.log(`the perimeter is ${perimeterResult2}`);
