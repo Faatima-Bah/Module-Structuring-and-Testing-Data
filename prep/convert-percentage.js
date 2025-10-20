2c09421c70e004bdbde6fdab7f372ed8c3e61f38
Fatimatou Bah <faatimabah@gmail.com>
2025-10-20 21:10:12 +0100

    added new file convert-percentage.js in the prep folder

--- prep/convert-percentage.js
+++ prep/convert-percentage.js
@@ -1,4 +1,10 @@
-const decimalNumber = 0.5;
-const percentage = `${decimalNumber * 100}%`;
+function convertToPercentage(decimalNumber) {
+    const percentage = `${decimalNumber * 100}%`;
+    return percentage;
+}
 
-console.log(percentage);
+const result1 = convertToPercentage(0.5);
+const result2 = convertToPercentage(0.231);
+
+console.log(result1);
+console.log(result2);
