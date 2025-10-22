b7585deac7cab7ae4c8015dece077037889bfa6f
Fatimatou Bah <faatimabah@gmail.com>
2025-10-22 23:46:27 +0100

    Sprint 1, key-exercise 3: answered

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,10 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex);
+const lastDotIndex = filePath.lastIndexOf(".");
+const ext = filePath.slice(lastDotIndex + 1);
 
+console.log(`The directory part of ${filePath} is ${dir}`);
+console.log(`The extension part of ${filePath} is ${ext}`);
 // https://www.google.com/search?q=slice+mdn
