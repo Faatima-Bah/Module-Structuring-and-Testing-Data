2f691f2f73735dee85152cf37472eb74b82c0c05
Fatimatou Bah <faatimabah@gmail.com>
2025-10-23 22:59:09 +0100

    4.js, mantory-errors exercise: answered

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twelveHourClockTime = "08:53";
+const twentyHourClockTime = "20:53";
+
+console.log(`${twelveHourClockTime} and ${twentyHourClockTime}`);
