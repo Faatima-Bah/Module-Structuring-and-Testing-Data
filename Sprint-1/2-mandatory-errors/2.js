576f4fbde1c7d9e47beb2ae429760449c20f3255
Fatimatou Bah <faatimabah@gmail.com>
2025-10-23 22:47:05 +0100

    2.js, mantory exercise: answered

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,5 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
+//The error was that the variable was declare after the statement. Which made the statement run first and bring up an error
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
